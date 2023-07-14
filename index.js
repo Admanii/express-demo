496784ce8bd9d33c8ec0e8fa26528e2e2b50b7c0
Ahmed Admani <ahmedriaz-50@hotmail.com>
2023-07-14 18:39:28 +0500

    Readme

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.get('/api/courses', (req,res)=> {
     res.send([1,2,3,4])
 })
 
+app.get('/api/courses/:years/:months', (req,res) => {
+    res.send(req.params);
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=> console.log(`Listening on port: ${port}..`))
